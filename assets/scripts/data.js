1058e329b0ed0398648fa4f8e566588b4b5d7614
Garv Shah <gshah.6110@gmail.com>
2025-01-05 04:11:43 +1100

    improved file organisation

---
+++ assets/scripts/data.js
@@ -0,0 +1,89 @@
+export const subjectDict = {
+    'accounting': {
+        'shortName': 'Accounting',
+        'tutors': []
+    },
+    'biology': {
+        'shortName': 'Biology',
+        'tutors': ['sophie']
+    },
+    'chemistry': {
+        'shortName': 'Chemistry',
+        'tutors': ['sophie']
+    },
+    'english': {
+        'shortName': 'English',
+        'tutors': []
+    },
+    'english-language': {
+        'shortName': 'EngLang',
+        'tutors': ['garv', 'sophie']
+    },
+    'geography': {
+        'shortName': 'Geography',
+        'tutors': ['sophie']
+    },
+    'global-politics': {
+        'shortName': 'GloPol',
+        'tutors': []
+    },
+    'legal-studies': {
+        'shortName': 'Legal',
+        'tutors': []
+    },
+    'literature': {
+        'shortName': 'Literature',
+        'tutors': []
+    },
+    'mathematical-methods': {
+        'shortName': 'Methods',
+        'tutors': ['garv', 'sophie']
+    },
+    'media': {
+        'shortName': 'Media',
+        'tutors': []
+    },
+    'physics': {
+        'shortName': 'Physics',
+        'tutors': ['garv']
+    },
+    'psychology': {
+        'shortName': 'Psychology',
+        'tutors': []
+    },
+    'software-development': {
+        'shortName': 'SoftDev',
+        'tutors': ['garv']
+    },
+    'specialist-maths': {
+        'shortName': 'Spec',
+        'tutors': ['garv']
+    },
+}
+
+export const tutorInfo = {
+    'garv' : {
+        'name': 'Garv Shah',
+        'atar': '99.50',
+        'subjects': {
+            'software-development': '45',
+            'mathematical-methods': '44',
+            'algorithmics': '39',
+            'english-language': '41',
+            'physics': '41',
+            'specialist-maths': '40',
+            'umep-maths': '4.5'
+        }
+    },
+    'sophie' : {
+        'name': 'Sophie Chang',
+        'atar': '99.05',
+        'subjects': {
+            'biology': '47',
+            'mathematical-methods': '42',
+            'geography': '43',
+            'chemistry': '41',
+            'english-language': '42'
+        }
+    },
+}
